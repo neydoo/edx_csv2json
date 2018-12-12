0ca62cc02c9db68950c53a19ccbe58e252fc3b16
Chinedu Ebinim <39536030+neydoo@users.noreply.github.com>
2018-12-12 11:28:50 +0100

    added tab stop

--- csv2json.js
+++ csv2json.js
@@ -9,9 +9,14 @@ const csv2json = (csvFile = './file/customer-data.csv') => {
         .fromFile(csvFile)
         .then((jsonObj) => {
             console.log('converting file..')
-            fs.writeFileSync(path.join(__dirname, 'file', 'customer-data.json'), JSON.stringify(jsonObj))
+            fs.writeFileSync(path.join(__dirname, 'file', 'customer-data.json'), JSON.stringify(jsonObj, null, "\t"), 'utf8', (error) => { //JSON.stringify converts from object to string
+                if (error) {
+                    console.log("error: " + error);
+                    return console.error(error);
+                } 
+                
+            })  
             console.log('file converted & saved succesfully.')
-            // console.log(jsonObj)
         })
 }
 
